35f4619e01df7d63acf0e6dde121241ba58924a7
Bendy <thanhtungnguyennnn@gmail.com>
2024-12-21 22:10:58 +0700

    Who wanna a Watermelon?

--- Assets/PLAYER/Script/The Watermalon/Watermelon.cs
+++ Assets/PLAYER/Script/The Watermalon/Watermelon.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Watermelon : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Watermelon setting")]
+    public int maxWatermelon = 5; //chua toi da 5 mieng dua hau
+    public int currentWatermelon = 0; //so luong dua hau hien tai
+    public int healAmount = 20; //luong mau hoi phuc
+
+    [Header("UI Elements")]
+    public Text watermelonCountText; //text hien thi so luong dua hau
+
+    private playersat playerStats; //tham chieu den chi so player
+
     void Start()
     {
-        
+        playerStats = FindObjectOfType<playersat>(); //lay sprite playersat
+
+        //Cap nhat UI
+        UpdateUI();
+    }
+    //tang so luong dua hau neu hhan duoc
+    public void AddWatermelon()
+    {
+        if(currentWatermelon < maxWatermelon)
+        {
+            currentWatermelon++;
+            UpdateUI();
+        }
+        else
+        {
+            Debug.Log("Da dat gioi han dua hau");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UseWatermelon()
     {
-        
+        if(currentWatermelon > 0 && playerStats.currenthp < playerStats.maxhp)
+        {
+            currentWatermelon--;
+            playerStats.currenthp += healAmount;
+
+            //dam bao mau khong vuot qua max hp
+            if(playerStats.currenthp > playerStats.maxhp)
+            {
+                playerStats.currenthp = playerStats.maxhp;
+            }
+
+            Debug.Log("Da su dung 1 watermelon");
+            UpdateUI();
+        }
+        else if(currentWatermelon == 0)
+        {
+            Debug.Log("Khong con dua hau de su dung");
+        }
+        else if(playerStats.currenthp >= playerStats.maxhp)
+        {
+            Debug.Log("Mau da day, khong can su dung");
+        }
+    }
+    private void UpdateUI()
+    {
+        //cap nhat so luong dua hau tren UI
+        if(watermelonCountText != null)
+        {
+            watermelonCountText.text = currentWatermelon.ToString();
+        }
     }
 }
